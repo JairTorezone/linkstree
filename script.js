17b3fcec9c82bc728993d9fa3cab4c8846c22b60
Jair Torezone <jairtorezone@gmail.com>
2023-01-23 20:53:03 -0300

    changing image with javascript

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ function toggleMode() {
   // }
 
   html.classList.toggle("light");
+
+  //pegar imagem
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Foto do jair na natureza");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Foto do Jair Torezone com chapéu e sorrindo");
+  }
 }
